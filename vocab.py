afba4b320c549a2afd7713386776fadc5be960a1
Harsimran Singh <me@harsimransingh.in>
2015-03-22 23:46:36 +0530

    updated copyright headers

--- vocab.py
+++ vocab.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python2
+#
+# vocab.py - allows the user to enter word and meaning
+#
+# Copyright (c) 2015 Harsimran Singh <me@harsimransingh.in>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 import sys, os, time
 
 
@@ -25,11 +45,11 @@ def get_word():
 
 def add_word(word, meaning):
     main_directory = os.path.join(os.path.expanduser("~"),  "wordlist")
-    print main_directory
+    # print main_directory
 
     # check if the directory exist
     main_directory = os.path.expanduser("~") + "/wordlist/"
-    print "main directory: ", main_directory
+    # print "main directory: ", main_directory
 
     if not os.path.exists(main_directory):
         os.makedirs(main_directory)
--- vocab_test.py
+++ vocab_test.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python2
+#
+# vocab_test.py - Quiz the user with the words stored
+#
+# Copyright (c) 2015 Harsimran Singh <me@harsimransingh.in>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 import os
 import sys
 import random
